f1a0abbe80638348116831ee485bc3eecf9ed5ed
raistah <bigkuraz@gmail.com>
2026-02-02 20:33:43 +0200

    fix: remove build dir

--- blocks/build/picture-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'a384f34d5d8b49c2d099');
